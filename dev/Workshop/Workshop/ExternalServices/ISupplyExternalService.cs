310e293eb21e6249e950677dfb7c7a4aa887612d
Itaigiliutin <giliutin@post.bgu.ac.il>
2022-04-19 21:23:51 +0300

    added skeleton for external services

--- dev/Workshop/Workshop/ExternalServices/ISupplyExternalService.cs
+++ dev/Workshop/Workshop/ExternalServices/ISupplyExternalService.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Workshop.DomainLayer.MarketPackage
+namespace Workshop.ExternalServices
 {
     interface ISupplyExternalService
     {
+        bool ExecuteTransaction(int id, int products, string Address);
     }
 }
