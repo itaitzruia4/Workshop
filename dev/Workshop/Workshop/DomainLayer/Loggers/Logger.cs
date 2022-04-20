c9e270518bb606a90a58f9f5227456ee9a4027d6
RonMichal <micron@post.bgu.ac.il>
2022-04-20 16:17:12 +0300

    Working logs

--- dev/Workshop/Workshop/DomainLayer/Loggers/Logger.cs
+++ dev/Workshop/Workshop/DomainLayer/Loggers/Logger.cs
@@ -1,11 +1,23 @@
-﻿namespace Workshop.DomainLayer.Loggers
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Workshop.DomainLayer.Loggers
 {
     public class Logger
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        public Logger()
+        private static readonly log4net.ILog log;
+        static Logger()
         {
-
+            if (log == null)
+            {
+                string fileName = "logs";
+                string basePath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath.Split(new string[] { "\\Logs" }, StringSplitOptions.None)[0];
+                string filePath = Path.Combine(basePath, $"Logs\\{fileName}.log");
+                log4net.GlobalContext.Properties["LogFileName"] = filePath; //Log file path
+                log4net.Config.XmlConfigurator.Configure();
+                log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            }
         }
 
         public void LogEvent(string message)
