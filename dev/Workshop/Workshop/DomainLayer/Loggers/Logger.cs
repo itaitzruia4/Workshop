6980a0dab550142514f91765acc8ab52313a6815
RonMichal <micron@post.bgu.ac.il>
2022-04-20 15:46:05 +0300

    Merged logger, added tests

---
+++ dev/Workshop/Workshop/DomainLayer/Loggers/Logger.cs
@@ -0,0 +1,26 @@
+﻿namespace Workshop.DomainLayer.Loggers
+{
+    public class Logger
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public Logger()
+        {
+
+        }
+
+        public void LogEvent(string message)
+        {
+            log.Info(message);
+        }
+
+        public void LogError(string message)
+        {
+            log.Error(message);
+        }
+
+        public void LogDebug(string mesesage)
+        {
+            log.Debug(mesesage);
+        }
+    }
+}
