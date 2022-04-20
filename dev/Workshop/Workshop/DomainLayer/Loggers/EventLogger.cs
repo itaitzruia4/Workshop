52fa8ca04768f5acea01defe5b08f12e97d722cd
RonMichal <micron@post.bgu.ac.il>
2022-04-20 14:08:41 +0300

    Added support for log4net

---
+++ dev/Workshop/Workshop/DomainLayer/Loggers/EventLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.DomainLayer.Loggers
+{
+    class EventLogger
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public EventLogger()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            log.Info("Testing!");
+            Console.ReadLine();
+        }
+    }
+}
