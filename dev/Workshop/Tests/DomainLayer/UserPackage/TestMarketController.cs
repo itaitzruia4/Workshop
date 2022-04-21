866037126eee2720e35b4d8ebad86d2273f72bea
Itaigiliutin <giliutin@post.bgu.ac.il>
2022-04-22 02:43:45 +0300

    added marketComtroller testing skeleton

---
+++ dev/Workshop/Tests/DomainLayer/UserPackage/TestMarketController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Workshop.DomainLayer.UserPackage;
+using Workshop.DomainLayer.UserPackage.Security;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestMarketController
+    {
+        private ISecurityHandler security;
+        private MarketController marketController;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var securityMock = new Mock<ISecurityHandler>();
+            securityMock.Setup(x => x.Encrypt(It.IsAny<string>())).Returns((string s) => s);
+            security = securityMock.Object;
+
+            marketController = new MarketController(security);
+        }
+
+        [TestMethod]
+        public void Temp_Success()
+        {
+            bool IHateMyLife = true;
+        }
+    }
+}
