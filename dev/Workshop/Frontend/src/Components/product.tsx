676e9442e60456a21db7f59320d64b5d66772761
Itaigiliutin <giliutin@post.bgu.ac.il>
2022-05-15 18:22:04 +0300

    started adding products to store page

--- dev/Workshop/Frontend/src/Components/product.tsx
+++ dev/Workshop/Frontend/src/Components/product.tsx
@@ -1,3 +1,4 @@
-export interface Product { tag: 'Product', id: number, name: string, basePrice: number, description: string, quantity: number };
+export type Product = {tag: "Product", id: number, name: string, basePrice: number, description: string, quantity: number };
+export type Products = { protducts: Product[], }
 export const makeProduct = (id: number, name: string, basePrice: number, description: string, quantity: number): Product =>
     ({ tag: "Product", id: id, name: name, basePrice: basePrice, description: description, quantity: quantity });
