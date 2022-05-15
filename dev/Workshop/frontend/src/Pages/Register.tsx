1a8b576017c779ece622904028b7d31c5e66100c
itaitzruia4 <itaitz@post.bgu.ac.il>
2022-05-15 18:35:02 +0300

    implemented date parsing on register

--- dev/Workshop/frontend/src/Pages/Register.tsx
+++ dev/Workshop/frontend/src/Pages/Register.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { isValidDate } from '../Utils/utils';
 import './Register.css';
+
+const DATE_FORMAT = 'MM/DD/YYYY';
 
 function Register() {
     const textStyle = { color: 'white' }
@@ -11,21 +14,54 @@ function Register() {
             navigate(path);
         }
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [birthDate, setBirthDate] = useState("");
+
+    function handleRegister(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        if (username === "" || password === "" || name === "" || birthDate == "") {
+            alert('User details must not be empty');
+            return;
+        }
+
+        var date_regex = /\d{1,2}\/\d{1,2}\/\d{4}/;
+        if (!(date_regex.test(birthDate))) {
+            alert("Invalid date format");
+            return false;
+        }
+
+        let url = "http://localhost:5165/api/authentication/register";
+
+        fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+                name: name,
+                birthDate: birthDate
+            })
+        });
+    }
+    
+
     return (
         <p className="register">
             <div className="register_title" style={textStyle}> Register </div>
             <p className="register_userInput">
                 <div style={textStyle}> Username: </div>
-                <input className="register_username_textbox" type="text" />
+                <input className="register_username_textbox" type="text" onChange={e => setUsername(e.target.value)} />
                 <div style={textStyle}> Password: </div>
-                <input className="register_password_textbox" type="password" />
+                <input className="register_password_textbox" type="password" onChange={e => setPassword(e.target.value)} />
                 <div style={textStyle}> Name: </div>
-                <input className="register_name_textbox" type="text" />
+                <input className="register_name_textbox" type="text" onChange={e => setName(e.target.value)} />
                 <div style={textStyle}> Birth Date: </div>
-                <input className="register_date_textbox" type="text" placeholder="dd/mm/yyyy" />
+                <input className="register_date_textbox" type="text" placeholder="dd/mm/yyyy" onChange={e => setBirthDate(e.target.value)} />
             </p>
             <p className="register_buttons">
-                <button className="register_register_btn"> Register </button>
+                <button className="register_register_btn" onClick={e => handleRegister(e)} > Register </button>
                 <button className="login_back_btn" onClick={routeChange('/')}> Back to home</button>
             </p>
         </p>
