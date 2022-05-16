9339dc31b5af307ff84d2eecbeaa013cf97d3c3f
Itaigiliutin <giliutin@post.bgu.ac.il>
2022-05-16 23:40:31 +0300

    fixed guest window design

--- dev/Workshop/frontend/src/Pages/Guest.tsx
+++ dev/Workshop/frontend/src/Pages/Guest.tsx
@@ -1,29 +1,130 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Guest.css';
+import { Store, Stores } from "../Components/store"
+import { Product } from "../Components/product"
+
+
+
+type Products = {
+    products: Product[],
+}
 
 function Register() {
-    const textStyle = { color: 'white' }
     let navigate = useNavigate();
     const routeChange = (path: string) =>
         () => {
             navigate(path);
         }
+
+    const [stores, setStores] = React.useState<Stores>({ stores: [] });
+
+    const [products, setProducts] = React.useState<Products>({ products: [{ tag: 'Product', id: 1, name: "candy", basePrice: 1000, description: "cool drug", quantity: 3 }] });
+    const deleteProducts = (id: number) => {
+        setProducts({
+            products: products.products.filter(t => t.id !== id),
+        });
+    };
     return (
-        <p className="guest">
-            <p className="guest_searchbars">
-                <input className="guest_search_product" type="text" placeholder="Search products" />
-            </p>
-            <p className="guest_cart">
-                <input className="guest_cart_scrollbar" type="text" placeholder="Search products" />
-            </p>
-            <p className="guest_buttons">
-                <button className="guest_register_btn" onClick={routeChange('/register') }> Register </button>
-                <button className="guest_login_btn" onClick={routeChange('/login')}> Login </button>
-                <button className="guest_exit_btn" onClick={routeChange('/')}> Exit Market </button>
-            </p>
-        </p>
-    )
+        <div className="guest_page">
+            <div className="guest_page_body">
+                <ConfigStoresComponent/>
+                <hr />
+                <div className="lists_section">
+                    <StoresComponent
+                        stores={stores}/>
+                    <CartComponent
+                        products={products}
+                        deleteProducts={deleteProducts} />
+                </div>
+                <p className="guest_control_btns">
+                    <button className="guest_exit_btn" onClick={routeChange('/')}> Exit Market </button>
+                </p>
+            </div>
+        </div>
+    );
 }
+
+
+const StoresComponent: React.FC<{
+    stores: Stores,
+}> = ({ stores }) => {
+
+    let navigate = useNavigate();
+    const routeChange = (path: string) =>
+        () => {
+            navigate(path);
+        }
+
+    return (
+        <div className="section__store">
+            <h2 className="stores-header">Stores</h2>
+            {stores.stores.length ? <ul className="stores">
+                {stores.stores.map(store => (
+                    <li key={store.id}>
+                        <span>{store.title}</span>
+                        <button
+                            className="guest_Open_Store_Btn"
+                            onClick={routeChange('/Store')}>Open store
+                        </button>
+                    </li>
+                ))}
+            </ul> : <div style={{ color: 'white' }} >No store have been found</div>}
+        </div>
+    );
+
+};
+
+const ConfigStoresComponent = () => {
+    const [store, setStore] = React.useState<string>("");
+    return (
+        <div className="Config_Store_Btns">
+            <form className="guest_Store_Form">
+                <button
+                    className="guest_Store_Btn"
+                   >Search product
+                </button>
+                <input className="guest_Store_textbox"
+                    value={store}
+                    onChange={e => { setStore(e.target.value) }} />
+            </form>
+        </div>
+    );
+};
+
+
+const CartComponent: React.FC<{
+    products: Products,
+    deleteProducts: (id: number) => void
+}> = ({ products, deleteProducts }) => {
+    const deleteProduct = (id: number) => {
+        if (window.confirm(`Are you sure you want to remove this product from your shopping cart?`)) {
+            deleteProducts(id);
+        }
+    }
+
+    return (
+        <div className="section__store">
+            <h2 className="stores-header">Shopping cart</h2>
+            {products.products.length ? <ul className="products">
+                {products.products.map(product => (
+                    <li key={product.id}>
+                        <span>{product.name}</span>
+                        <button
+                            className="guest_Delete_Srore_Btn"
+                            onClick={() => { deleteProduct(product.id) }}>X
+                        </button>
+                    </li>
+                ))}
+            </ul> : <div style={{ color: 'white' }} >Your shopping cart is empty</div>}
+            <button
+                className="guest_Buy_Btn"
+            >Buy cart
+            </button>
+        </div>
+    );
+
+};
+
 
 export default Register;
