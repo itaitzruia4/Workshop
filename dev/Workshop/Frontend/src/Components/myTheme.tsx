52a8ea7ba07c44d4d52ae63b3c325f0c1968f199
Itaigiliutin <giliutin@post.bgu.ac.il>
2022-05-25 13:15:58 +0300

    added nested expanded list for each store

---
+++ dev/Workshop/Frontend/src/Components/myTheme.tsx
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@mui/material/styles';
+
+const dark = createMuiTheme({
+    palette: {
+        text: {
+            primary: "white",
+            secondary: "white"
+        }
+    }
+});
+
+export default dark;
--- dev/Workshop/Frontend/src/Components/storesList.tsx
+++ dev/Workshop/Frontend/src/Components/storesList.tsx
@@ -12,6 +12,12 @@ import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import StarBorder from '@mui/icons-material/StarBorder';
 import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import { useState } from 'react';
+
 
 export default function StoresList(name: string) {
     const [open, setOpen] = React.useState(true);
@@ -20,6 +26,47 @@ export default function StoresList(name: string) {
         setOpen(!open);
     };
 
+    const CustomizedListItem: React.FC<{
+        store: string
+    }> = ({ store }) => {
+        const [open, setOpen] = useState(false)
+        const handleClick = () => {
+            setOpen(!open)
+        }
+
+        return (
+            <div>
+                <ListItem button key={store} onClick={handleClick}>
+                    <ListItemText primary={<Typography style={{ color: 'white' }}>{store}</Typography>} />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                    <ListItemIcon>
+                        <StorefrontIcon />
+                    </ListItemIcon>
+                </ListItem>
+                <Collapse
+                    key={store}
+                    in={open}
+                    timeout='auto'
+                    unmountOnExit
+                >
+                    <List component='li' disablePadding key={store}>
+                        {store.split('').map(sheet => {
+                            return (
+                                <ListItem button key={sheet}>
+                                    <ListItemIcon>
+                                        <ProductionQuantityLimitsIcon />
+                                    </ListItemIcon>
+                                    <ListItemText key={sheet} primary={<Typography style={{ color: 'white' }}>{sheet}</Typography>} />
+                                </ListItem>
+                            )
+                        })}
+                    </List>
+                </Collapse>
+                <Divider />
+            </div>
+        )
+    }
+/*
     return (
 <List sx={{ width: '100%', maxWidth: '100%', bgcolor: 'background.paper' }}
       component="nav"
@@ -40,5 +87,17 @@ export default function StoresList(name: string) {
                 ))}
 
         </List>
+    );
+    */
+    return (
+        <div>
+            <List component='nav' aria-labelledby='nested-list-subheader'>
+                {["drugs", "snacks", "candy"].map(doc => {
+                    return (
+                        <CustomizedListItem store={doc} />
+                    )
+                })}
+            </List>
+        </div>
     );
 }
