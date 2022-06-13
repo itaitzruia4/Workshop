8826fa19819b2b1443d3b45b02c785b1c75f68e5
itaitzruia4 <itaitz@post.bgu.ac.il>
2022-06-13 11:23:25 +0300

    test

--- dev/Workshop/frontend/src/Pages/Member.tsx
+++ dev/Workshop/frontend/src/Pages/Member.tsx
@@ -25,7 +25,7 @@ import { Product } from "../Types/product"
 import { Cart, Bag } from '../Types/shopping';
 import { MarketNotification } from '../Types/Notification';
 
-
+// Member page
 function Member() {
     const [refreshKey, setRefreshKey] = useState(0);
 
