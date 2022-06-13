2ea5b0459689fe2ecd47f5247f4def1b67d1fdee
Itaigiliutin <giliutin@post.bgu.ac.il>
2022-06-13 15:10:34 +0300

    added basic profile page

--- dev/Workshop/Frontend/src/Pages/Profile.tsx
+++ dev/Workshop/Frontend/src/Pages/Profile.tsx
@@ -1,36 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { handleEnterMarket } from '../Actions/AuthenticationActions';
 import Button from '@mui/material/Button';
-import { makeUserToken, userToken } from '../Types/roles';
 
+import { makeUserToken, memberToken } from '../Types/roles';
+import { Store } from "../Types/store"
+import { Product } from "../Types/product"
+import { Cart, Bag } from '../Types/shopping';
+import { MarketNotification } from '../Types/Notification';
 
 
 function Profile() {
 
-
     const textStyle = { color: 'black' }
+    const [refreshKey, setRefreshKey] = useState(0);
+
+    const location = useLocation();
+    const token = location.state as memberToken;
 
     let navigate = useNavigate();
-    const routeChange = (path: string, token: userToken) =>
-        () => {
-            console.log("navigating from enter market to login page, user id:", token.userId)
+    const routeChange = (path: string, token: memberToken) =>
+        () =>
             navigate(path, { state: token });
-        }
 
     return (
         <p className="welcome">
-            <div className="welcome_title" style={textStyle}> Welcome to the Trading System website! </div>
+            <div className="welcome_title" style={textStyle}> {token.membername + "'s profile" } </div>
             <p className="welcome_buttons">
-                <Button variant="contained"
-                    onClick={() =>
-                        handleEnterMarket()
-                            .then(value => { console.log("enter market user id:", value); return value; })
-                            .then(value => routeChange('/login', makeUserToken(value as number))())
-                            .catch(error => {
-                                alert("Couldnt connect to server")
-                            })
-                    }> Enter Market </Button>
+                <Button variant="contained"> Test </Button>
+                <Button variant="contained" onClick={routeChange('/member', token) }> Back to main window </Button>
             </p>
         </p>
     )
